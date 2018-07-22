0a750c1c8fc3e92c4c1354d628433c74c84ac64d
Michael Kuehl <michael@ratchet.cc>
2018-07-22 20:01:40 +0200

    added a mongodb loader image

--- scripts/create_imagestream.sh
+++ scripts/create_imagestream.sh
@@ -5,8 +5,10 @@ oadm new-project base-images --display-name='base-images' --description='Standar
 
 oc create -f images/java/base-java-imagestream.yaml -n base-images
 oc create -f images/java-mvn/java-mvn-imagestream.yaml -n base-images
+oc create -f images/mongodb-loader/mongodb-loader-imagestream.yaml -n base-images
 
 oc start-build base-java -n base-images
 oc start-build java-mvn -n base-images
+oc start-build mongodb-loader -n base-images
 
 oadm policy add-role-to-group system:image-puller system:authenticated -n base-images
